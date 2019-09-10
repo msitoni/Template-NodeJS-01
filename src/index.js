28d9da7ebec53fb429779f726af6fbf36126b5f0
marcio <msitoni@hotmail.com>
2019-09-10 17:26:46 -0300

    Arquivo de funcoes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import { converterFormatoNumeroBrasileiroParaAmericano, converterFormatoNumeroAmericanoParaBrasileiro } from './util/Util'
+
+
 const a = 10;
-console.log(a);
+
+console.log(converterFormatoNumeroBrasileiroParaAmericano(a));
