7a8dd5cf1871536843657e476b2ae098fbf6957a
marcio <msitoni@hotmail.com>
2019-09-10 17:21:50 -0300

    Arquivo de funcoes

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,4 +1,4 @@
-brTOus = function(number) {
+const converterFormatoNumeroBrasileiroParaAmericano = (number) => {
     !number ? number = "0,00" : number = number;
     if (number.toString().indexOf(',') == -1) {
         return parseFloat(number);
@@ -9,10 +9,14 @@ brTOus = function(number) {
 };
 
 
-function usTObr(n) {
-    n = parseFloat(n);
-    if (!n) {
-        n = 0;
+const converterFormatoNumeroAmericanoParaBrasileiro = (number) => {
+    !number ? number = "0,00" : number = number;
+    number = parseFloat(n);
+    if (!number) {
+        number = 0;
     }
-    return parseFloat(n).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+    return parseFloat(number).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
 };
+
+
+export { converterFormatoNumeroBrasileiroParaAmericano, converterFormatoNumeroAmericanoParaBrasileiro };
